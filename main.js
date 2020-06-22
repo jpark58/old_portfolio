1e4a026266b15b693e3f3b15fc95f143f27f993c
Jinseo <jinseopark94@gmail.com>
2020-06-22 11:09:43 +0900

    JS: Navbar menu

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Make navbar trasparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
+// Handle scrolling when tappingon the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
